868874b1a08362427c2bded145baa2ad10fdf794
Andrew Bishop <andrew.bishop53@gmail.com>
2018-10-16 16:19:21 -0700

    added dismiss buttons, using component state

--- src/App.js
+++ src/App.js
@@ -2,39 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// commands: npm start, npm test, npm run build
+
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: ' Jordan Walke ',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: ' Dan Abramov, Andrew Clark ',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+];
+
+const helloWorld = "Welcome to the Road to Learn React, ";
+
+const user = 'Bud';
+
 class App extends Component {
-  render() {
-    const helloWorld = "Welcome to the Road to Learn React, ";
-		let user = {
-			first: "Andrew",
-			last: "Bishop"
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list
     };
-    function formatName (user) {
-      return user.first + ' ' + user.last;
-    }
-    const list = [
-      {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-      },
-      {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-      },
-    ];
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    // console.log(this.state.list);
+    // console.log(updatedList);
+    this.setState({list: updatedList});
+    //this.forceUpdate();
+  }
+
+  render() {
+    
     return (
       <div className="App">
         <img src={logo} height="100" width="100" alt="react logo"/>
-        <h2>{helloWorld} {formatName(user)}</h2>
-        {list.map(item =>
+        <h2>{helloWorld} {user}</h2>
+        {this.state.list.map(item =>
             <div key={item.objectID}>
                   <span>
                     <a href={item.url}>{item.title}</a>
@@ -42,7 +60,14 @@ class App extends Component {
                   <span>{item.author}</span>
                   <span>{item.num_comments}</span>
                   <span>{item.points}</span>
-                  
+                  <span>
+                    <button 
+                      onClick={() => this.onDismiss(item.objectID)}
+                      type="button"
+                    >
+                      Dismiss
+                    </button>
+                  </span>
             </div>
         )}
       </div>
